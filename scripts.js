8def7921f966068e980501c1480735d4493041cd
shellygsicat@gmail.com <shellygsicat@gmail.com>
2016-10-21 23:44:37 -0600

    Added textarea in the editor section with codemirror

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
+/* Editor (utilizing Code Mirror) Functions */
+window.onload = function () {
+  var editableCodeMirror = CodeMirror.fromTextArea(document.getElementById('editorTextArea'), {
+    mode: "html",
+    theme: "default",
+    lineNumbers: true
+  });
+}
+
+/* Drag and Drop Functions */
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
 
 function onDrag(ev) {
-    /* //On Drag 
+    /* //On Drag
     var code = "<example> Hello World </example>";
     ev.dataTransfer.setData("text", ev.code); */
     ev.dataTransfer.setData("text", ev.target.id);

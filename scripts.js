ef118e952b2cae2343f67315c9e7c44b7044ac7f
shellygsicat@gmail.com <shellygsicat@gmail.com>
2016-10-26 17:38:02 -0600

    Expand toolboxes for languages complete

--- scripts.js
+++ scripts.js
@@ -25,37 +25,3 @@ function onDrop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data).cloneNode(true));
 }
-
-var dragAndDrop = {
-
-    limit: 4,
-    count: 0,
-
-    init: function () {
-        this.dragula();
-        this.eventListeners();
-    },
-
-    eventListeners: function () {
-        this.dragula.on('drop', this.dropped.bind(this));
-    },
-
-    dragula: function () {
-        this.dragula = dragula([document.querySelector('#left'), document.querySelector('#right')],
-        {
-            moves: this.canMove.bind(this),
-            copy: true,
-        });
-    },
-
-    canMove: function () {
-        return this.count < this.limit;
-    },
-
-    dropped: function (el) {
-        this.count++;
-    }
-
-};
-
-dragAndDrop.init();
